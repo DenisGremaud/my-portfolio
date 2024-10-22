1ec1344865b2e0aea52e882b31c0b5cf5d62caa8
Denis Gremaud <32323238+DenisGremaud@users.noreply.github.com>
2024-10-22 16:26:45 +0200

    Update home text and link

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,16 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Denis Gremaud
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Récent diplômé en ingénierie informatique de l'HEPIA, je suis passionné par le développement web et l'intelligence artificielle. 
+		Ma formation m'a doté d'une solide compréhension des fondements de l'informatique et de compétences pratiques avancées. 
+		Je suis maintenant prêt à mettre en application mes connaissances techniques et ma passion pour l'innovation au sein d'une entreprise dynamique. 
+		Enthousiaste à l'idée de contribuer à des projets stimulants, je cherche des opportunités pour appliquer et développer mes compétences en développement logiciel, en particulier dans les domaines du web et de l'IA.`}
       </p>
       <div className="my-8">
-        <BlogPosts />
+        {/* <BlogPosts /> */}
       </div>
     </section>
   )
