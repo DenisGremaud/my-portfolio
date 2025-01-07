5d1d074bb3fd79fd2f80ba9721bec15699b9f66f
Denis Gremaud <32323238+DenisGremaud@users.noreply.github.com>
2025-01-07 16:03:29 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,14 @@ export const metadata: Metadata = {
     siteName: 'Denis Gremaud | Portfolio',
     locale: 'fr_CH',
     type: 'website',
+	images: [
+	  {
+		url: 'icon.png',
+		width: 192,
+		height: 192,
+		alt: 'Denis Gremaud | Portfolio',
+	  },
+	]
   },
   twitter: {
     card: 'summary_large_image',
