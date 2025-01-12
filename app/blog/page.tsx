d34280d9c3a3d762d66f4eccafff5d84903ac841
Denis Gremaud <32323238+DenisGremaud@users.noreply.github.com>
2025-01-12 18:52:38 +0100

    update margin on blog post h1

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,7 +8,9 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Blog Posts</h1>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter mt-4">
+        Blog Posts
+      </h1>
       <BlogPosts />
     </section>
   )
